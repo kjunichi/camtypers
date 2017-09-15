ae7b79c36f94fc1b5a2420c1ec23d9548f26a793
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-09-15 20:38:37 +0900

    Remove unused code

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn dmprint(str: &Vec<u8>) {
     }
     print!("\n");
 }
+
 fn atoi(num: String) -> i32 {
     // println!("num = {}, {}", num, num.len());
     num.parse().expect("not a number")
@@ -33,7 +34,7 @@ fn find_char(buf: &Vec<u8>, target: u8, len: i32) -> i32 {
     return -1;
 }
 
-fn strncpy(mut line: &mut Vec<u8>, buf: &Vec<u8>, pos: i32) {
+fn strncpy(line: &mut Vec<u8>, buf: &Vec<u8>, pos: i32) {
     for i in 0..pos {
         line[i as usize] = buf[i as usize];
     }
@@ -51,7 +52,7 @@ fn read_line_(buf: &Vec<u8>, line: &mut Vec<u8>, len: i32) -> i32 {
 }
 
 fn read_line(buf: &mut Vec<u8>, line: &mut Vec<u8>, len: i32) -> i32 {
-    let mut tmpbuf = buf;
+    let tmpbuf = buf;
     let mut n: i32 = 0;
 
     loop {
@@ -78,8 +79,7 @@ fn get_image_size(buf: &mut Vec<u8>) -> (i32, i32) {
         tmp_str.push(' ' as u8);
     }
     let pos: i32 = find_char(&buf, ' ' as u8, buf.len() as i32);
-    // println!("pos = {}", pos);
-    let mut ptr = &mut tmp_str;
+    let ptr = &mut tmp_str;
     strncpy(ptr, buf, pos);
     (*ptr)[pos as usize] = '\0' as u8;
     // dmprint(ptr);
@@ -102,68 +102,29 @@ fn get_image_size(buf: &mut Vec<u8>) -> (i32, i32) {
 }
 
 fn parse_ppm_header(ppmbuf: &mut Vec<u8>, len: i32) -> (i32, i32, i32) {
-    // print!("ppmbuf = [");
-    // for i in 0..12 {
-    // print!("{}", (*ppmbuf)[i as usize] as char);
-    // }
-    // println!("]");
     let mut line: Vec<u8> = Vec::with_capacity(len as usize);
     for _ in 0..len {
         line.push(' ' as u8);
     }
-    // println!("ppmbuf.len = {}", (*ppmbuf).len());
-    let mut tmpbuf = &mut *ppmbuf;
-    // print!("tmpbuf = [");
-    // for i in 0..12 {
-    // print!("{}", (*tmpbuf)[i as usize] as char);
-    // }
-    // println!("]");
-    //
+    let tmpbuf = &mut *ppmbuf;
     let mut header_length: i32 = 0;
 
     // PNM TYPE
-    let mut lineptr = &mut line;
+    let lineptr = &mut line;
     let mut n = read_line(tmpbuf, lineptr, 1024);
-    //     println!("n = {}", n);
-    // print!("lineptr = [");
-    // for i in 0..n {
-    // print!("{}", (*lineptr)[i as usize] as char);
-    // }
-    // println!("]");
-    // print!("tmpbuf = [");
-    // for i in 0..12 {
-    // print!("{}", (*tmpbuf)[i as usize] as char);
-    // }
-    // println!("]");
-    //
     header_length += n;
     n = read_line(tmpbuf, lineptr, 1024);
-    //     println!("n = {}", n);
-    // print!("tmpbuf = [");
-    // for i in 0..12 {
-    // print!("{}", (*tmpbuf)[i as usize] as char);
-    // }
-    // println!("]");
-    //
     header_length += n;
-    //     print!("lineptr = [");
-    // for i in 0..n - 1 {
-    // print!("{}", (*lineptr)[i as usize] as char);
-    // }
-    // println!("]");
-    //
-    // lineptr = &mut line;
     let (width, height) = get_image_size(lineptr);
 
     n = read_line(tmpbuf, lineptr, 1024);
 
-    // depth = atoi(line);
     header_length += n;
     return (header_length, width, height);
 }
 
 fn parse_ppm_p6(buf: &mut Vec<u8>, len: i32) -> Ppm {
-    let mut ptr = &mut *buf;
+    let ptr = &mut *buf;
     let (pos, width, height) = parse_ppm_header(ptr, len);
     Ppm {
         width: width,
@@ -174,16 +135,13 @@ fn parse_ppm_p6(buf: &mut Vec<u8>, len: i32) -> Ppm {
 
 fn main() {
     imgtypers::term_init();
-    let name = "WebCam test";
     let capture = webcamrs::webcam::create_camera_capture(0);
-    webcamrs::webcam::named_window(name);
+
     loop {
         let frame = webcamrs::webcam::query_frame(&capture);
-        webcamrs::webcam::show_image(name, &frame);
-
         let params = vec![];
         let mut mat = webcamrs::webcam::encode_image(".ppm", &frame, params);
-        let mut matptr = &mut mat.buf;
+        let matptr = &mut mat.buf;
         let ppminfo = parse_ppm_p6(matptr, mat.cols);
         for _ in 0..(ppminfo.index) {
             (*matptr).remove(0);
@@ -191,25 +149,10 @@ fn main() {
         imgtypers::term_put_image(matptr, ppminfo.width, ppminfo.height);
         imgtypers::term_flush();
 
-        let c = webcamrs::webcam::wait_key(2);
-        if c == 0x1b {
+        if imgtypers::term_get_esc_key() {
             break;
-        }
-        if c == 0x20 {
-            webcamrs::webcam::save_image("snap.jpg", &frame);
-            // break;
-            let params = vec![];
-            let mut mat = webcamrs::webcam::encode_image(".ppm", &frame, params);
-            let mut matptr = &mut mat.buf;
-            let ppminfo = parse_ppm_p6(matptr, mat.cols);
-            for _ in 0..(ppminfo.index) {
-                (*matptr).remove(0);
-            }
-            imgtypers::term_put_image(matptr, ppminfo.width, ppminfo.height);
-            imgtypers::term_flush();
         }
     }
     webcamrs::webcam::release_capture(&capture);
-    webcamrs::webcam::destroy_all_windows();
     imgtypers::term_close();
 }
